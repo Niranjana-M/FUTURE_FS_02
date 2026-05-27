4f8840f7dff36e0666e8a754bacb81f1268ae259
Niranjana M <niranjanam276@gmail.com>
2026-05-28 01:15:41 +0530

    fixed login credentials

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,25 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-// LOGIN ROUTE
 router.post("/login", (req, res) => {
   const { email, password } = req.body;
 
   console.log("LOGIN:", email, password);
 
-  // SIMPLE STATIC LOGIN
   if (email === "admin@gmail.com" && password === "123456") {
     return res.json({
       message: "Login successful",
       user: {
         name: "Admin",
-        email: "admin@gmail.com"
-      }
+      },
     });
   }
 
   return res.status(401).json({
-    message: "Invalid credentials"
+    message: "Invalid credentials",
   });
 });
 
